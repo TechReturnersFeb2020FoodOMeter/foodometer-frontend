d87cdecc36971cde3c59a311cc0f08196f7e4e52
preethis89 <preethis89@outlook.com>
2020-05-21 18:05:37 +0100

    Added a default route

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ export default class App extends Component {
               )}
             />
             <Route
-              exact
+             
               path={"/dashboard"}
               render={(props) => (
                 <Dashboard
