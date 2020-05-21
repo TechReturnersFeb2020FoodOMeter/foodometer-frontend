64bed391c9bbdc81d93ff42ffcb2f3853a586cf5
preethis89 <preethis89@outlook.com>
2020-05-21 16:46:45 +0100

    Updates

---
+++ precache-manifest.dc4c21e131823c5011810d2663000b5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9950a8ff882984e8cbca012faea8ef38",
+    "url": "/foodometer-frontend/index.html"
+  },
+  {
+    "revision": "f5740ba9fd8c8a0a3b48",
+    "url": "/foodometer-frontend/static/css/main.d54c882c.chunk.css"
+  },
+  {
+    "revision": "12d43301a7d25b0790af",
+    "url": "/foodometer-frontend/static/js/2.852a90a4.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/foodometer-frontend/static/js/2.852a90a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5740ba9fd8c8a0a3b48",
+    "url": "/foodometer-frontend/static/js/main.298a5c83.chunk.js"
+  },
+  {
+    "revision": "719c098538c7c675e8c0",
+    "url": "/foodometer-frontend/static/js/runtime-main.ddb5ce44.js"
+  },
+  {
+    "revision": "10c4da590d8f5bade04240cd1060f145",
+    "url": "/foodometer-frontend/static/media/brooke-lark-08bOYnH_r_E-unsplash.10c4da59.jpg"
+  },
+  {
+    "revision": "10cfec1e78235fc5b8810974672d0663",
+    "url": "/foodometer-frontend/static/media/cyan-canvas.10cfec1e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a44f2aee14a9097a62bc3bd1a946655.js"
+  "/foodometer-frontend/precache-manifest.dc4c21e131823c5011810d2663000b5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/foodometer-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
